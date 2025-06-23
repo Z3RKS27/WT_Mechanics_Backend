16e4737ccf29dade2ca27a9ae0efdcd35b562ff1
Z3RKS27 <145464913+Z3RKS27@users.noreply.github.com>
2025-06-22 21:55:57 -0600

    env

--- config/database.js
+++ config/database.js
@@ -27,9 +27,9 @@ module.exports = ({ env }) => {
         connectionString: env('DATABASE_URL'),
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'wt_mechanics'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '198723'),
+        database: env('DATABASE_NAME', 'wt_mechanics_h9q3'),
+        user: env('DATABASE_USERNAME', 'wt_mechanics_h9q3_user'),
+        password: env('DATABASE_PASSWORD', 'KRzx7Il4GHTbuZrLz2vvMyWdwJmgWpqK'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
