166b9d45fb95a25ee22eda0e93057d2f98f58482
Z3RKS27 <145464913+Z3RKS27@users.noreply.github.com>
2025-06-25 09:42:03 -0600

    middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
     config: {
       origin: ['http://localhost:3000',
         'http://192.168.0.3:3000',
-        'https://wt-mechanics-nld8-13hunqmuu-z3rks27s-projects.vercel.app',
+        'https://wt-mechanics-nld8-b0etzdg35-z3rks27s-projects.vercel.app',
       ],
       // Opciones adicionales si las necesitas:
       headers: ['*'],
